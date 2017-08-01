b63d37099423769e1700dc02ba7268dc09eb91f3
cristineshipman <cristine.shipman@gmail.com>
2017-08-01 15:22:20 -0500

    partials

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -1,29 +1,31 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 
 // ROUTES
 
-//homepage
+//Root Route
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 //you fell in love with
 app.get("/fallinlovewith/:thing", function(req, res){
   var thing = req.params.thing;
-  res.render("love.ejs", {thingVar: thing});
+  res.render("love", {thingVar: thing});
 });
 
 //POSTS
 app.get("/posts", function(req, res){
   var posts = [
-    {title: "Post 1", author: "Susy"},
-    {title: "Post 2", author: "Susy"},
+    {title: "Post 1", author: "Cristine"},
+    {title: "Post 2", author: "Ben"},
     {title: "Post 3", author: "Susy"}
   ];
-  res.render("posts.ejs", {posts: posts})
+  res.render("posts", {posts: posts})
 });
 
 //callback that will run once the server is listening
